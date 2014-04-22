c40400c800e6b3b755deb7b566973c9f255266f0
gianordoli <gianordoligabriel@yahoo.com.br>
2014-04-22 17:51:42 -0400

    Tried to add globs, without success

--- content_scripts/js/main.js
+++ content_scripts/js/main.js
@@ -1,9 +1,9 @@
 // Checking page title
-if (document.title.indexOf("Google") != -1) {
+// if (document.title.indexOf("Google") != -1) {
     //Creating Elements
     var btn = document.createElement("BUTTON")
     var t = document.createTextNode("CLICK ME");
     btn.appendChild(t);
     //Appending to DOM 
     document.body.appendChild(btn);
-}
+// }
