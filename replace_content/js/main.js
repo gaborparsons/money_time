c0be34adb0e5394cafb3b0642005934002aaaed2
gianordoli <gianordoligabriel@yahoo.com.br>
2014-04-23 09:39:16 -0400

    Selecting values from string
    Looking for monetary values, extracting and parsing them into floats.

--- replace_content/js/main.js
+++ replace_content/js/main.js
@@ -1,1 +1,24 @@
 $( "div:contains('$')" ).css( "text-decoration", "underline" );
+
+var moneyDivs = $("div:contains('$')");
+for(var i = 0; i < moneyDivs.length; i++){
+    // console.log(money[i]);
+    var text = moneyDivs[i].innerHTML;
+    // console.log(text);
+    
+    var textSlice = text.substring(text.indexOf('$'), text.length);
+    var index = 1;
+    var moneyValue = '';
+
+    //add characters to the string:
+    //if it is a number
+    //if it is a dot
+    //until the end of the original string
+    while((!isNaN(textSlice[index]) || textSlice[index] == '.') && index < textSlice.length){
+        moneyValue += textSlice[index];
+        index ++;
+    }
+    moneyValue = parseFloat(moneyValue);
+    console.log(moneyValue);
+    // console.log(moneyValue);
+}
